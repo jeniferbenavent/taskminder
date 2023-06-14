30934205cbf49191bca463ffad97470054c6840b
jeniferbenavent <jbenaventzapata@gmail.com>
2023-06-14 16:19:10 +0200

    Working on Login page

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,13 +1,28 @@
-import { TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import './LoginForm.css';
 
 function LoginForm() {
   return (
-    <form className='login-form'>
-      <TextField id="outlined-basic" label="Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Surname" variant="outlined" />
-      <TextField id="outlined-basic" label="Surname" variant="outlined" />
-    </form>
+    <div className='rightPanel'>
+      <div className='infoTitle'>
+        <h1>Login</h1>
+        <p>Welcome Back! Please login to your account.</p>
+      </div>
+      <form className='login-form'>
+        <div className="form-group">
+          <TextField className="form-input" id="outlined-basic" label="Username" variant="outlined" />
+        </div>
+        <div className="form-group">
+          <TextField className="form-input" id="outlined-basic" label="Email" variant="outlined" />
+        </div>
+        <div className="form-group">
+          <TextField className="form-input" id="outlined-basic" label="Password" variant="outlined" />
+        </div>
+        <div className="form-group">
+          <Button type="submit" variant="contained">LogIn</Button>
+        </div>
+      </form>
+    </div>
   );
 }
 
