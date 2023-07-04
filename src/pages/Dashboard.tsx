a731586735cab036251677a42f0ae6caa30541f2
jeniferbenavent <jbenaventzapata@gmail.com>
2023-07-04 09:41:17 +0200

    Validating LogIn Form... need some fixes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import SideMenu from "../components/SideMenu/SideMenu"
-import ToDoList from "../components/ToDoList/TodoList"
+import ToDoList from "../components/ToDoList/ToDoList"
 
 const Dashboard = () => {
 
